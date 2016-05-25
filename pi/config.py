f8641f03c6089619422a97829d8f5aec23371461
Vladimir Magamedov <vladimir@magamedov.com>
2016-05-26 00:05:33 +0300

    Using simplest yaml loader for safety reasons and for simplicity

--- pi/config.py
+++ pi/config.py
@@ -1,12 +1,12 @@
 import codecs
 import os.path
 
-import yaml
+import yaml.loader
 
 
 def read_config():
     config = {}
     if os.path.exists('pi.yaml'):
         with codecs.open('pi.yaml', encoding='utf-8') as f:
-            config = yaml.load(f.read()) or {}
+            config = yaml.load(f.read(), yaml.loader.BaseLoader) or {}
     return config
