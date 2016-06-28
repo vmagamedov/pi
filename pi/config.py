a6f1f7c23543f7d10ab3314f84444f312d9a8f22
Vladimir Magamedov <vladimir@magamedov.com>
2016-06-28 15:28:39 +0300

    Using sorted OrderedDict instead of "dict" when parsing YAML config to properly hash it

--- pi/config.py
+++ pi/config.py
@@ -1,13 +1,27 @@
 import codecs
 import os.path
+from operator import itemgetter
+from collections import OrderedDict
 
 from ._requires.yaml import load as yaml_load
 from ._requires.yaml import loader as yaml_loader
+from ._requires.yaml.resolver import BaseResolver
 
 from . import types
 
 
 class Loader(yaml_loader.SafeLoader):
+
+    def construct_mapping(self, node, deep=False):
+        items = list(super().construct_mapping(node, deep=False).items())
+        items.sort(key=itemgetter(0))
+        return OrderedDict(items)
+
+    def construct_yaml_map(self, node):
+        data = OrderedDict()
+        yield data
+        value = self.construct_mapping(node)
+        data.update(value)
 
     @classmethod
     def register(cls, type_):
@@ -18,6 +32,9 @@ class Loader(yaml_loader.SafeLoader):
         for enum_value in enum:
             cls.add_constructor(enum_value.value, enum.construct)
 
+
+Loader.add_constructor(BaseResolver.DEFAULT_MAPPING_TAG,
+                       Loader.construct_yaml_map)
 
 Loader.register(types.Meta)
 Loader.register(types.Image)
