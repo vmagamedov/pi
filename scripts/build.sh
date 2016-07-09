ff20c599cc0b854a577a252014b35e34f96967a6
Vladimir Magamedov <vladimir@magamedov.com>
2016-07-09 16:51:13 +0300

    Added optimizations to the Python build process to reduce size. Using semantic versioning style to create first pre-release.

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,10 +11,19 @@ cd "/Python-${PYTHON_VERSION}" \
   && cp /in/ModulesSetup.local ./Modules/Setup.local \
   && ./configure --prefix=/opt/python --disable-shared LDFLAGS="-static" CFLAGS="-static" CPPFLAGS="-static" \
   && make \
-  && make install || echo 'ignore'
+  && make install || true
 
 cd /opt/python/lib/python2.7 \
+  && rm -rf ./test \
+  && rm -rf ./lib2to3/tests \
+  && /opt/python/bin/python2.7 -O -m compileall -f -q . \
+  && find . -name "*.py" -type f -delete \
+  && find . -name "*.pyc" -type f -delete \
   && zip -r /opt/python/lib/python2.7.zip *
+
+curl -fSL https://github.com/lalyos/docker-upx/releases/download/v3.91/upx -o /bin/upx \
+  && chmod +x /bin/upx \
+  && /bin/upx /opt/python/bin/python2.7
 
 cp /opt/python/bin/python2.7 /out/_python
 cp /opt/python/lib/python2.7.zip /out/stdlib.zip
--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='Pi',
-    version='0.1.dev0',
+    version='0.1-dev.1',
     description='Command-line interface for your projects',
     author='Vladimir Magamedov',
     author_email='vladimir@magamedov.com',
     url='https://github.com/vmagamedov/pi',
     packages=find_packages(),
+    include_package_data=True,
     license='BSD',
     install_requires=[],
     entry_points={
