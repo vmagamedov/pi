4b1a3a9fb4d9fe7320ba2afd0f77ca640cebfd04
Vladimir Magamedov <vladimir@magamedov.com>
2017-03-25 07:47:16 +0200

    Added part of the forward compatibility - ability to load unknown tagged values from YAML config

--- pi/config.py
+++ pi/config.py
@@ -1,16 +1,37 @@
 import codecs
 import os.path
+
 from operator import itemgetter
 from collections import OrderedDict
 
-from ._requires.yaml import load as yaml_load
-from ._requires.yaml import loader as yaml_loader
+
+from ._requires import yaml
+from ._requires.yaml import SafeLoader
+from ._requires.yaml.nodes import ScalarNode, SequenceNode, MappingNode
 from ._requires.yaml.resolver import BaseResolver
 
 from . import types
 
 
-class Loader(yaml_loader.SafeLoader):
+class Unknown:
+
+    def __init__(self, loader, suffix, node):
+        self.tag = suffix
+        if isinstance(node, ScalarNode):
+            obj = loader.construct_scalar(node)
+        elif isinstance(node, SequenceNode):
+            obj = loader.construct_sequence(node, True)
+        elif isinstance(node, MappingNode):
+            obj = loader.construct_mapping(node, True)
+        else:
+            raise TypeError(repr(node))
+        self.obj = obj
+
+    def __repr__(self):
+        return '!{}({!r})'.format(self.tag, self.obj)
+
+
+class Loader(SafeLoader):
 
     def construct_mapping(self, node, deep=False):
         items = list(super().construct_mapping(node, deep=deep).items())
@@ -35,6 +56,7 @@ class Loader(yaml_loader.SafeLoader):
 
 Loader.add_constructor(BaseResolver.DEFAULT_MAPPING_TAG,
                        Loader.construct_yaml_map)
+Loader.add_multi_constructor('!', Unknown)
 
 Loader.register(types.Meta)
 Loader.register(types.Image)
@@ -46,14 +68,14 @@ Loader.register(types.Service)
 Loader.register(types.LocalPath)
 Loader.register(types.NamedVolume)
 Loader.register(types.Expose)
-Loader.register_enum(types.Mode)
 Loader.register(types.Download)
 Loader.register(types.Bundle)
+Loader.register_enum(types.Mode)
 
 
 def read_config():
     config = {}
     if os.path.exists('pi.yaml'):
         with codecs.open('pi.yaml', encoding='utf-8') as f:
-            config = yaml_load(f.read(), Loader) or {}
+            config = yaml.load(f.read(), Loader) or {}
     return config
