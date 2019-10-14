3f2d1fdc8a790095da93fad1fc45f9c49c42ea29
Vladimir Magamedov <vladimir@magamedov.com>
2019-10-14 07:40:43 +0300

    wip1

--- pi/status.py
+++ pi/status.py
@@ -2,37 +2,13 @@ import sys
 from collections import defaultdict
 
 
-def _f(s):
-    sys.stdout.write(s)
-    sys.stdout.flush()
-
-
-def _move(from_, to):
-    count = to - from_
-    if count > 0:
-        _down(count)
-    elif count < 0:
-        _up(-count)
-
-
-def _up(count):
-    _f(f'{chr(27)}[{count}A')
-
-
-def _down(count):
-    _f(f'{chr(27)}[{count}B')
-
-
-def _erase():
-    _f(f'{chr(27)}[2K\r')
-
-
 class Status:
     def __init__(self):
         self._idx = []
         self._steps = defaultdict(list)
         self._titles = {}
         self._current = 0
+        self._file = sys.stdout
 
     def __enter__(self):
         return self
@@ -40,9 +16,30 @@ class Status:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.finish()
 
+    def _write(self, s):
+        self._file.write(s)
+        self._file.flush()
+
+    def _up(self, count):
+        self._write(f'{chr(27)}[{count}A')
+
+    def _down(self, count):
+        self._write(f'{chr(27)}[{count}B')
+
+    def _erase(self):
+        self._write(f'{chr(27)}[2K\r')
+
     def _move(self, to):
-        _move(self._current, to)
+        count = to - self._current
+        if count > 0:
+            self._down(count)
+        elif count < 0:
+            self._up(-count)
         self._current = to
+
+    def _newline(self):
+        self._write('\n')
+        self._current += 1
 
     def add_task(self, title):
         self._move(len(self._idx))
@@ -50,42 +47,41 @@ class Status:
         key = object()
         self._titles[key] = title
         self._idx.append(key)
-        _f(title)
+        self._write(title)
 
-        # new line
-        _f('\n')
-        self._current += 1
+        self._newline()
         return key
 
     def add_step(self, task_key, title):
-        after_key = self._steps[task_key][-1] if task_key in self._steps else task_key
+        if task_key in self._steps:
+            append_after = self._steps[task_key][-1]
+        else:
+            append_after = task_key
 
-        pos = self._idx.index(after_key) + 1
+        insert_pos = self._idx.index(append_after) + 1
 
         key = object()
-        self._idx.insert(pos, key)
+        self._idx.insert(insert_pos, key)
         self._steps[task_key].append(key)
         self._titles[key] = title
 
-        self._move(pos)
-        _erase()
-        _f(title)
-        for i in range(pos + 1, len(self._idx)):
+        self._move(insert_pos)
+        self._erase()
+        self._write(title)
+        for i in range(insert_pos + 1, len(self._idx)):
             self._move(i)
-            _erase()
-            _f(self._titles[self._idx[i]])
+            self._erase()
+            self._write(self._titles[self._idx[i]])
 
-        # new line
-        _f('\n')
-        self._current += 1
+        self._newline()
         return key
 
     def update(self, key, title):
         self._move(self._idx.index(key))
-        _erase()
-        _f(title)
+        self._erase()
+        self._write(title)
         self._titles[key] = title
 
     def finish(self):
         self._move(len(self._idx))
-        _erase()
+        self._erase()
