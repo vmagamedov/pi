38066fa98087169b686cf777497596b336d04d67
Vladimir Magamedov <vladimir@magamedov.com>
2020-03-03 19:15:42 +0200

    Issued 0.1.2rc3 release candidate

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='pi-env',
-    version='0.1.2rc2',
+    version='0.1.2rc3',
     description='Command-line interface for your projects',
     author='Vladimir Magamedov',
     author_email='vladimir@magamedov.com',
     url='https://github.com/vmagamedov/pi',
     packages=find_packages(),
     include_package_data=True,
-    license='BSD',
+    license='BSD-3-Clause',
+    python_requires='>=3.7',
     install_requires=[],
     entry_points={
         'console_scripts': ['pi=pi.__main__:main'],
